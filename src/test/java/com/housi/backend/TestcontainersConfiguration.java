cb1c58397a47441d5dfebb99aa0f888a333ac226
Christos Chousiadas <41087223+Housiadas@users.noreply.github.com>
2026-05-17 18:25:26 +0300

    Setup redis (#3)

--- src/test/java/com/housi/backend/TestcontainersConfiguration.java
+++ src/test/java/com/housi/backend/TestcontainersConfiguration.java
@@ -7,19 +7,26 @@ import org.testcontainers.grafana.LgtmStackContainer;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import com.redis.testcontainers.RedisContainer;
+
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-	@Bean
-	@ServiceConnection
-	LgtmStackContainer grafanaLgtmContainer() {
-		return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:latest"));
-	}
+    @Bean
+    @ServiceConnection
+    LgtmStackContainer grafanaLgtmContainer() {
+        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:latest"));
+    }
 
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer postgresContainer() {
-		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
-	}
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer postgresContainer() {
+        return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+    }
 
+    @Bean
+    @ServiceConnection(name = "redis")
+    RedisContainer redisContainer() {
+        return new RedisContainer(DockerImageName.parse("redis:latest"));
+    }
 }
